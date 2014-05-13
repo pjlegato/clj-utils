bedcce35f22c241f751f93bea6e8b9771dbad133
Paul Legato <pjlegato@builtonlogic.com>
2014-05-12 22:04:20 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# clj-utils
+
+General purpose Clojure utilities.
+
+## License
+
+Copyright © 2014 Paul Legato. All rights reserved.
---
+++ src/com/paullegato/clj_utils/core.clj
@@ -0,0 +1,120 @@
+(ns com.paullegato.clj-utils.core
+  "General Clojure utility functions"
+  (:require [onelog.core :as log]))
+
+
+(defn path-to-filename
+  "Given a path, returns only the filename portion of it."
+  [long-path]
+  (last  (clojure.string/split long-path #"/")))
+
+
+(defn- milli-time
+  "Returns System/nanoTime converted to milliseconds."
+  []
+  (long (/ (System/nanoTime) 1000000)))
+
+
+(defn spin-on*
+  "Spins at most n times waiting for f to be true. Each spin is timeout
+  ms long. Throws an Exception upon timeout with the given tag added to
+  its message."
+  [f tag n timeout]
+  (log/debug "Spinlocking on " tag)
+  (loop [n n]
+    (if (f)
+      (log/debug "Spinlock released for " tag)
+      (if (> n 0)
+        (do (Thread/sleep timeout)
+            (recur (- n 1)))
+        (throw (Exception. (str "Spinlock timeout awaiting " tag)))))))
+
+
+(defmacro spin-on
+  "Spins at most n times waiting for f to be true. Each spin is
+  timeout ms long.
+
+  Adds the current filename and line to the exception generated upon
+  timeout, for easier debugging."
+  [f n timeout]
+  `(spin-on* ~f
+             ~(str (pr-str f) " at " (path-to-filename *file*) ":" (:line (meta &form)))
+             ~n
+             ~timeout))
+
+
+(defmacro with-timeout 
+  "Runs the given code, aborting it and throwing an exception with the
+  given forms in its message after ms milliseconds if the forms have not
+  finished executing yet."
+  [ms & forms]
+  `(let [f# (future ~@forms)]
+     (try
+       (.get ^java.util.concurrent.Future f# ~ms java.util.concurrent.TimeUnit/MILLISECONDS)
+       (catch java.util.concurrent.TimeoutException t#
+         ;; Re-throw another exception with a more helpful message containing
+         ;; the code that timed out, wrapping the original exception.
+         (throw (RuntimeException.
+                 (str "Timed out while waiting up to " ~ms " ms for the following code to run: " '~@forms)
+                 t#))))))
+
+
+(defn has-keys? [m keys]
+  (every? (partial contains? m) keys))
+
+
+(defn uuid
+  "Generates a random UUID."
+  []
+  (java.util.UUID/randomUUID))
+
+
+(defmacro <!!-timeout
+  "Like core.async's <!!, but times out and logs a warning after the
+  given number of ms have passed if nothing could be read from port during that time."
+  [port ms]
+  `(let [timeout-port#  (async/timeout ~ms)
+         [val# port#]   (alts!! [~port timeout-port#])]
+    (if (= port# timeout-port#)
+      (log/warn+ (log/color [:bright :yellow]
+                            "<!!-timeout timed out while trying to read " '~port "!")))
+    val#))
+
+
+(defn add-shutdown-hook!*
+  "Causes the given function to be called when the JVM is shut down,
+  such as when the user presses Ctrl-C at the console, the code calls
+  System.exit(), etc..
+
+  Note that this is on a best-effort basis; it is not guaranteed that the
+  function will be called in all cases before the process shuts down."
+  [f]
+  (.addShutdownHook (Runtime/getRuntime) (Thread. f)))
+
+
+(defmacro add-shutdown-hook!
+  "Runs the given code on a best-effort basis when the JVM is shut down."
+  [& forms]
+  `(add-shutdown-hook!* (fn [] ~@forms)))
+
+
+(defn set-uncaught-exception-handler!*
+  "Sets a JVM-wide handler for uncaught exceptions. Calls the given
+  function with two arguments, the thread that threw the exception, and
+  the throwable that wasn't caught."
+  [f]
+  (Thread/setDefaultUncaughtExceptionHandler
+   (reify Thread$UncaughtExceptionHandler
+     (uncaughtException [this thread throwable]
+       (f thread throwable)
+       (log/error "Default JVM exception logger: got an uncaught exception from " thread "!")
+       (log/error throwable)))))
+
+
+(defmacro set-uncaught-exception-handler!
+  "Sets a JVM-wide handler for uncaught exceptions. The given forms will
+  be run with 'thread and 'throwable bound appropriately"
+  [& forms]
+  `(set-uncaught-exception-handler!*
+    (fn [~'thread ~'throwable]
+      ~@forms)))
